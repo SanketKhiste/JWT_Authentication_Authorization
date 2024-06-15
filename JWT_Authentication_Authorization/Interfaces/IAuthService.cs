0eb6cc078f77cef79f072f393d83cb0e7b1b9750
SanketKhiste <khistesanket19@gmail.com>
2024-06-15 22:56:21 +0530

    Initial

---
+++ JWT_Authentication_Authorization/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using JWT_Authentication_Authorization.Models;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace JWT_Authentication_Authorization.Interfaces
+{
+    public interface IAuthService
+    {
+        User AddUser(User user);
+        string Login(LoginReq loginRequest);
+        Role AddRole(Role role);
+        bool AssignRoleToUser(AddUserRole obj);
+
+    }
+}
